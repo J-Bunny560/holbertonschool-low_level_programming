55096df8cde33c66f057b75e98f7cbb809ac4b79
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-02-19 20:25:08 -0400

    writing a program that writes a specific message

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,6 +5,6 @@
  *Return: Always 0.
  */
 int main(void)
-{puts("\"Programing is like building a multilingual puzzle ");
-return (0);
-}
+{puts("\"Programing is like building a multilingual puzzle");
+return (0); \n
+} 

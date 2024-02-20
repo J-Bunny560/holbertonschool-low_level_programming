89a8a6d81473d5d59e09c89d2cad70c659a5a895
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-02-19 21:31:43 -0400

    writing a program that writes a specific message

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
-
 /**
-*main - Entry poit
-*
-*Return: Always 0 (Success)
+ *main - Entry poit
+ *
+ *Return: Always 0 (Success)
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0) ;
+return ( 0 ) ;
 }

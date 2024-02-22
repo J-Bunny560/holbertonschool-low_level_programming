b2e1ff829d6b545fe5bfb5f0056885108ba5de14
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-02-21 21:38:43 -0400

    controls a character

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  *_islower - entry point
  *Description: Controls if a character is in lowercase
- *@c character to be verified 
+ *@c: character to be verified
  *Return: return 0 or 1
  */
 int _islower(int c)

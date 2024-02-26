929200a888970ce98f37f97f983e8b17b8ec0559
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-02-26 10:46:02 -0400

    swaps the values of two intergers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two intergers
+ * @a: pointer to first value
+ * @b: pointer to second value
+ */
+void swap_int(int *a, int*b)
+{
+int c;
+
+c =a *a;
+*a = *b;
+*b = c;
+}

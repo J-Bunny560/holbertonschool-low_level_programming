8ec99df169045c08adf0fa23f05cba487c82dcb5
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-03-11 12:37:38 -0400

    library command add

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,17 +6,18 @@
  * @age: age of the dog
  * @owner: name of the owner of the dog
  *
- * Description: Defines metadata about the dog and the type pf dog's struct
+ * Description: Defines metadata about the dog and type of dog's struct
  */
 typedef struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 } dog_t;
 /* Additional function prototypes */
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif

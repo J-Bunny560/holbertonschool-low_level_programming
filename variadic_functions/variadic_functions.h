0cd4d2f5f59c8448448ea326b0448176ff581305
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-03-14 12:30:31 -0400

    Function that sums all arguments

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,12 +1,12 @@
-#ifndef VARIADIC_FNS
-#define VARIADIC_FNS
-
-#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+#ifndef VARIADIC_H
+#define VARIADIC_H
+#define VALIDTYPESCONDITIONAL(i)(format[i] == 'c'\
+|| format[i] == 'i' format[i] == 'f' || format[i] == 's')
 
 typedef struct validTypes
 {
-	char *valid;
-	void (*f)();
+char *valid;
+void (*f)();
 } v_types;
 
 int _putchar(char c);

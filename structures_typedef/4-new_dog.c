022aeec083c0f5c85a639d053a84e52abee5c686
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-03-15 09:23:56 -0400

    Function that prints anything

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -21,18 +21,18 @@ doggy = malloc(sizeof(dog_t));
 if (doggy == NULL)
 return (NULL);
 
-doggy -> name = malloc(nlen *sizeof(doggy -> name));
+doggy->name = malloc(nlen *sizeof(doggy->name));
 if (doggy == NULL)
 return (NULL);
 for (i = 0; i < nlen; i++)
-doggy -> name[i] = name[i];
+doggy->name[i] = name[i];
 
-doggy -> age = age;
+doggy->age = age;
 
-doggy -> owner = malloc(olen * sizeof(doggy -> owner));
+doggy->owner = malloc(olen * sizeof(doggy->owner));
 if (doggy == NULL)
 return (NULL);
 for (i = 0; i < olen; i++)
-doggy -> owner[i] = owner[i];
+doggy->owner[i] = owner[i];
 return (doggy);
 }

2a3826cfd0ba2aa085661788375b475f937f6a4c
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-03-15 09:40:20 -0400

    Function that prints anything

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,30 +9,29 @@
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int nlen, olen, i;
 dog_t *doggy;
 
-nlen = olen = 0;
-while (name[nlen++])
-;
-while (owner[olen++])
-;
 doggy = malloc(sizeof(dog_t));
-if (doggy == NULL)
+if (doggy = NULL)
 return (NULL);
 
-doggy->name = malloc(nlen *sizeof(doggy->name));
-if (doggy == NULL)
+doggy->name = strdup(name);
+if (doggy->name = NULL)
+{
+free(doggy);
 return (NULL);
-for (i = 0; i < nlen; i++)
-doggy->name[i] = name[i];
+}
 
 doggy->age = age;
 
-doggy->owner = malloc(olen * sizeof(doggy->owner));
-if (doggy == NULL)
+doggy->owner = strdup(owner);
+if (doggy->owner = NULL)
+{
+free(doggy->name);
+free(doggy);
 return (NULL);
-for (i = 0; i < olen; i++)
-doggy->owner[i] = owner[i];
+}
+
 return (doggy);
 }
+

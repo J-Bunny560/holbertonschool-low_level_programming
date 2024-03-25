da1d6d9acd0d6975c84eff9bc277e175a76c0501
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-03-25 16:04:41 -0400

    function that frees a list_t list

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,37 @@
 #include "lists.h"
 #include <stdlib.h>
+/**
+  * _strlen - gets length of the string
+  * @s: string
+  * Return: length of the string
+  */
+int _strlen(const char *s)
+{
+	int i;
 
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+/**
+  * _strdup - recreation of string duplicate function
+  * @src: source of string to duplicate
+  * Return: pointer to malloc'd space with copied string
+  */
+void *_strdup(const char *src)
+{
+	int len, i;
+	char *dest;
+
+	len = _strlen(src);
+	dest = malloc((len + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
 /**
  * add_node_end - add new node to the end of list_t
  * @head: pointer to the pointer of beginning of list_t

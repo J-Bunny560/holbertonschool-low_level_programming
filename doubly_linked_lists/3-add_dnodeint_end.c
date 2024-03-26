9648aa24ca98ee7a02534bf17179a0884b42f44a
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-03-26 16:20:39 -0400

    functiion that writes a node at the end of list

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,15 +12,16 @@ dlistint_t *new_node, *last_node;
 
 new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
-return NULL;
+return (NULL);
 
 new_node->n = n;
 new_node->next = NULL;
 
-if (*head == NULL) {
+if (*head == NULL)
+{
 new_node->prev = NULL;
 *head = new_node;
-return new_node;
+return (new_node);
 }
 
 last_node = *head;
@@ -29,5 +30,5 @@ last_node = last_node->next;
 last_node->next = new_node;
 new_node->prev = last_node;
 
-return new_node;
+return (new_node);
 }

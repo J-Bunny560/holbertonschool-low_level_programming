f19005a73c05b45337c1ab5a3e0a63e6b95e14a3
Joshua Maldonado <joshuamaldonado66gmail.com>
2024-03-26 15:01:42 -0400

    functiion that writes a node at the beginning of list

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_dnodeint - add new nodes to the list
+ * @head: current place in the list
+ * @n: int to add to the list
+ * Return: pointer to current position on list
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *new;
+
+new = *head;
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = *head;
+*head = new;
+return (*head);
+}
